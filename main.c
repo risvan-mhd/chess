3c054ba2885c4620f0d3e4847f98720cf243880b
Risvan Muhammad <risvan145k@gmail.com>
2026-05-23 23:08:24 +0530

    feat: draw board

--- main.c
+++ main.c
@@ -4,6 +4,23 @@
 #define HEIGHT 800
 #define TITLE "Chess"
 
+#define CELL_SIZE (WIDTH / 8)
+#define DARK_CELL_COLOR ((Color){118, 150, 86, 255})
+#define LIGHT_CELL_COLOR ((Color){238, 238, 210, 255})
+
+
+void draw_board(void) {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            int x = i * CELL_SIZE;
+            int y = j * CELL_SIZE;
+            Color color = (i + j) % 2 == 0 ? LIGHT_CELL_COLOR : DARK_CELL_COLOR;
+
+            DrawRectangle(x, y, CELL_SIZE, CELL_SIZE, color);
+        }
+    }
+}
+
 
 int main(void) {
     InitWindow(WIDTH, HEIGHT, TITLE);
@@ -11,8 +28,11 @@ int main(void) {
 
     while (!WindowShouldClose()) {
         BeginDrawing();
+        ClearBackground(BLACK);
+        draw_board();
         EndDrawing();
     }
 
+    CloseWindow();
     return 0;
 }
